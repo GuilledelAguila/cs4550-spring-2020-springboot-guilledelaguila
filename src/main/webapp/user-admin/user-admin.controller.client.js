5ef70d8492f1ca7e38f055bff9d0c86b7ff4be86
Guillermo del Aguila <gdelagui@purdue.edu>
2020-01-26 19:33:32 -0500

    implemented functionality to display all users when page is loaded

--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -8,7 +8,39 @@
     var $removeBtn, $editBtn, $createBtn, $updateBtn;
     var selectedUser
 
+
     let currentUserIndex = -1
+
+    function findAllUsers() {
+        userService
+            .findAllUsers()
+            .then(theusers => {
+                users = theusers
+                renderUsers(users)
+            })
+    }
+    function renderUsers(users) {
+        $tbody.empty()
+        for(let u in users) {
+            renderUser(u)
+
+        }
+    }
+    function renderUser(u) {
+        let user = users[u]
+        let rowClone = $userRowTemplate.clone()
+        rowClone.removeClass('wbdv-hidden')
+        rowClone.find('.wbdv-username').html(user.username)
+        rowClone.find('.wbdv-last-name').html(user.lastName)
+        rowClone.find('.wbdv-first-name').html(user.firstName)
+        rowClone.find('.wbdv-role').html(user.role)
+        $removeBtn = rowClone.find('.wbdv-remove')
+        $removeBtn.click(() => deleteUser(u))
+        $editBtn = rowClone.find('.wbdv-edit')
+        $editBtn.click(() => findUserById(u))
+
+        $tbody.append(rowClone)
+    }
 
     function main(){
         $usernameFld = $('#usernameFld')
@@ -21,10 +53,10 @@
         $userRowTemplate= $('.wbdv-template')
 
 
-        $createBtn = $('.wbdv-create')
-        $createBtn.click(createUser)
-        $updateBtn = $('.wbdv-update')
-        $updateBtn.click(updateUser)
+        // $createBtn = $('.wbdv-create')
+        // $createBtn.click(createUser)
+        // $updateBtn = $('.wbdv-update')
+        // $updateBtn.click(updateUser)
 
 
 
